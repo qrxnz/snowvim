12b86f05473f9681f759f20d206f30131e9609a2
qrxnz <send@qrxnz.dev>
2024-10-25 22:12:00 +0200

    catppuccin theme

--- config/theming/default.nix
+++ config/theming/default.nix
@@ -1,6 +1,15 @@
-{
+{ lib, config, ... }:{
   # Import all your configuration modules here
-  imports = [ 
+  imports = [
     ./lualine.nix
+    ./catppuccin.nix
   ];
+
+  options = {
+    colorschemes.enable = lib.mkEnableOption "Enable colorschemes module";
+  };
+  
+  config = lib.mkIf config.colorschemes.enable {
+    catppuccin.enable = lib.mkDefault true;
+  };
 }
